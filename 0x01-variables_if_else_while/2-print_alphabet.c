f3e68d2f796ed1afabe6903a912c4312e0538b7e
billy <attiawilfried@gmail.com>
2022-09-24 08:19:34 -0700

    Getting alphabet letter in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Function to print alphabet letter in lower case */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	putchar('\n');
+	return(0);
+}

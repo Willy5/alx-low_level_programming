74abb9b35f6d4e3db197a51c0ce9073ed204498a
billy <attiawilfried@gmail.com>
2022-09-26 04:38:34 -0700

    Printing alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** Program to print alphabet in reverse order */
+int main()
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
